433623e9114048de82cd2dcc29b874e9ea0b3402
Salitha <salithachathuranga94@gmail.com>
2022-07-21 12:40:04 +0530

    Add new project

---
+++ src/main/java/Project/to/demonstrate/String/Boot/validation/library/and/exception/handling/SpringBootValidationAndExceptionsApplication.java
@@ -0,0 +1,13 @@
+package Project.to.demonstrate.String.Boot.validation.library.and.exception.handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationAndExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootValidationAndExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/to/demonstrate/String/Boot/validation/library/and/exception/handling/SpringBootValidationAndExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.to.demonstrate.String.Boot.validation.library.and.exception.handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationAndExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
